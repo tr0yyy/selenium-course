d0dcdf58560250dd31ea474a172de828c326b26e
anicoi <anicoi@bitdefender.biz>
2022-04-28 09:41:09 +0300

    wip curs 1

---
+++ src/main/java/curs1/TestNgExample.java
@@ -0,0 +1,46 @@
+package curs1;
+
+import org.testng.annotations.*;
+
+public class TestNgExample {
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After suite");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before class");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before method");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After method");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test 2");
+	}
+}
